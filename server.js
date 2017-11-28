0543db56af94237c4e9361331925a51b7e18e1c9
MatanHu <MatanHuja@gmail.com>
2017-11-28 18:53:33 +0200

    Add LTC

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var bit2c = require('bit2c');
 var B2C_BTC = require('./B2C/B2C_BTC');
 var B2C_BCH = require('./B2C/B2C_BCH');
+var B2C_LTC = require('./B2C/B2C_LTC');
 var EmailFactory = require('./Factories/emailFactory');
 var FcmSender = require('./Factories/FCM');
 
@@ -113,6 +114,58 @@ function getB2C_BCH() {
     });
 }
 
+function getB2C_LTC() {
+    bit2c.getTicker('LtcNis', function (error, ticker) {
+
+        try {
+            console.log('LtcNis Start at: ' + new Date());
+            if (error) {
+                console.log(error);
+            } else {
+                // console.log(ticker);
+                var rate = {
+                    sellPrice: ticker.l,
+                    buyPrice: ticker.h,
+                    date: new Date()
+                };
+                B2C_LTC.getLastRate(function (err, rows) {
+                    // console.log(rows);
+                    if (err) {
+                        console.error('error is: ' + err);
+                    } else {
+                        // if (rows && !rows.length || (rows[0].sellPrice != rate.sellPrice || rows[0].buyPrice != rate.buyPrice)) {
+                            if(rows && !rows.length || (Math.abs(rows[0].sellPrice - rate.sellPrice) > 10 || Math.abs(rows[0].buyPrice - rate.buyPrice) > 10)) {
+                            B2C_LTC.addRate(rate, function () { console.log('Databes Inserted'); });
+                            if (rows[0]) {
+                                checkTwoNumbers('LtcNis', rows[0], rate);
+                                // FcmSender.sendFcm('LtcNis', rows[0], rate);
+                            }
+                        }
+                    }
+                });
+                if (ticker.h < price) {
+                    console.log(' The Price to Buy is: ' + ticker.h + ' Low of: ' + price);
+                }
+                if (ticker.h > price) {
+                    console.log(' The Price to Buy is: ' + ticker.h + ' Over of: ' + price);
+                }
+                if (ticker.l < price) {
+                    console.log(' The Price to Sell is: ' + ticker.l + ' Low of: ' + price);
+                }
+                if (ticker.l > price) {
+                    console.log(' The Price to Sell is: ' + ticker.l + ' Over of: ' + price);
+                }
+                console.log('LtcNis End at: ' + new Date());
+                console.log('');
+                console.log('');
+            }
+        } catch (ex) {
+            console.error('Exception: ' + ex);
+        }
+
+    });
+}
+
 function checkTwoNumbers(cryptoType, lastRate, newRate) {
     if (lastRate.buyPrice > 10000 || newRate.buyPrice > 10000 || lastRate.sellPrice > 10000 || newRate.sellPrice > 10000) {
         if ((Math.floor(lastRate.buyPrice / 1000) != Math.floor(newRate.buyPrice / 1000)) ||
@@ -148,3 +201,4 @@ function checkTwoNumbers(cryptoType, lastRate, newRate) {
 
 setInterval(getB2C_BTC, 1000);
 setInterval(getB2C_BCH, 1000);
+setInterval(getB2C_LTC, 1000);
